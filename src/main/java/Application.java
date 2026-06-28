85f62afc647802d17fe13a7b9a184765eeb7260d
Alex Russell <128105099+AlexVRussell@users.noreply.github.com>
2026-06-28 17:05:49 -0300

    initial commit

---
+++ src/main/java/Application.java
@@ -0,0 +1,11 @@
+package com.example.application;
+
+import org.springframwork.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpingBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
